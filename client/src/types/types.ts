5e3a05e9314b1a7d86df0c239235ce0191828e8a
tushermahmud <s@overmind.gg>
2024-02-18 18:22:46 +0600

    Showing data from gift cards

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,25 +1,49 @@
 export type User = {
-    id: number;
-    role: string;
-    email: string;
-    username: string;
-    password: string;
-}
+  id: number;
+  role: string;
+  email: string;
+  username: string;
+  password: string;
+};
 
 export enum UserType {
-    ADMIN = 'admin',
-    USER = 'user'
+  ADMIN = "admin",
+  USER = "user",
 }
 
 export type Question = {
-    id: number;
-    question: string;
-    answer: string;
-    options: string[];
-}
+  id: number;
+  question: string;
+  answer: string;
+  options: string[];
+};
 
 export type Answer = {
-    questionId: number;
-    answer: string;
-}
+  questionId: number;
+  answer: string;
+};
 
+export type CatalogueItem = {
+  cardItemId: string;
+  id: string;
+  name: string;
+  cardFaceImage: string;
+  giftCardInformation: string;
+  value: number;
+};
+
+export type Order = {
+    customerName: string,
+    firstName: string,
+    lastName: string,
+    referenceNo: string,
+    deliveryChannel: string,
+    contactNumber: string,
+    smsMobileNumber: string,
+    emailAddress: string,
+    additionalParameters: Record<string, never>,
+    countryCode: string,
+    languageCode: string,
+    orderDate: string,
+    lineItems: CatalogueItem[]
+  }
